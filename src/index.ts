e38f24f6bec3a0fea2dd2f80dcecba71eb8d9c5a
Your Name <catalinmiro093@gmail.com>
2025-11-01 18:40:46 +0200

    Remove leftover game folders

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-// Note: This file was moved to `ts-backend/src/index.ts` to use the proper tsconfig and dependencies.
-// Keeping this placeholder avoids stray TypeScript errors in editors that open `src/index.ts`.
-export {};
